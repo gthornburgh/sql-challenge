c234bf4c6226a44ffb6c514ec73534cdddd5a3c6
gthornburgh <grantthornburgh@gmail.com>
2021-01-16 18:35:35 -0500

    commit 17

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -84,3 +84,11 @@ JOIN dept_manager
 ON departments.dept_no = dept_manager.dept_no
 JOIN employees
 ON dept_manager.emp_no = employees.emp_no
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no

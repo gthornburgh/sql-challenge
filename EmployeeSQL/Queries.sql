651e62b07661830e4954ce62bc9147b94190fdad
gthornburgh <grantthornburgh@gmail.com>
2021-01-16 18:34:57 -0500

    commit 16

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -76,3 +76,11 @@ ON employees.emp_no = salaries.emp_no
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01'
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no

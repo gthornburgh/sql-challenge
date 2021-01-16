6a78dab72af6faa3b6552aa9f246a42ab80f039f
gthornburgh <grantthornburgh@gmail.com>
2021-01-16 18:36:41 -0500

    commit 19

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -98,3 +98,12 @@ SELECT first_name, last_name
 FROM employees
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%'
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales'

7e859de20168372c6f049b7289733c2dd5ca1410
gthornburgh <grantthornburgh@gmail.com>
2021-01-16 18:32:30 -0500

    commit 12

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -55,3 +55,6 @@ REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
